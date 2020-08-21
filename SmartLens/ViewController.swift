68ff4faa2cee6714dfebf6e25a118aab16950f76
kishlaychhajer <kishlay.chhajer@alumni.ubc.ca>
2020-08-21 20:03:37 +0530

    Initial Commit

---
+++ SmartLens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SmartLens
+//
+//  Created by Kishlay Chhajer on 2020-08-21.
+//  Copyright © 2020 Kishlay Chhajer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
