f21f06e8c1d1d1c591d8acb2edb6fcae7b9c50e6
kishlaychhajer <kishlay.chhajer@alumni.ubc.ca>
2020-08-22 14:54:57 +0530

    Initial commit

--- SmartLens/ViewController.swift
+++ SmartLens/ViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import AVKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, UINavigationControllerDelegate {
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let session = AVCaptureSession()
+        if let device = AVCaptureDevice.default(for: .video) {
+            if let input = try? AVCaptureDeviceInput(device: device) {
+                session.addInput(input)
+                session.startRunning()
+                let preview = AVCaptureVideoPreviewLayer(session: session)
+                view.layer.addSublayer(preview)
+                preview.frame = view.frame
+                let output = AVCaptureVideoDataOutput()
+                output.setSampleBufferDelegate(self, queue: DispatchQueue(label: "dispatchQueue"))
+                session.addOutput(output)
+        }
+    }
+        
+    }
+    
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        if let pixel : CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) {
+            if let model = try? VNCoreMLModel(for: Resnet50().model) {
+            let request = VNCoreMLRequest(model: model) { (requests, error) in
+                if let results = requests.results as? [VNClassificationObservation] {
+                    DispatchQueue.main.async {
+                        self.navigationItem.title = results.first?.identifier
+                    }
+                    
+                }
+                }
+            try? VNImageRequestHandler(cvPixelBuffer: pixel, options: [:]).perform([request])
+            }
+        }
     }
 
 
